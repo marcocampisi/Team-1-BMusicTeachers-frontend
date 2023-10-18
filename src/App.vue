6673763370359782e99d910b06a137173825d8a9
marcocampisi <marco.campisi@outlook.it>
2023-10-18 11:12:01 +0200

    Add axios call

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,11 @@
 <script setup>
+import HeaderComponent from './components/HeaderComponent.vue';
+import MainComponent from './components/MainComponent.vue';
 </script>
 
 <template>
-    <div>
-        <a href="https://vitejs.dev" target="_blank">
-            <img src="/vite.svg" class="logo" alt="Vite logo" />
-        </a>
-        <a href="https://vuejs.org/" target="_blank">
-            <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-        </a>
-        <h1>Vite + Vue</h1>
-    </div>
+    <HeaderComponent />
+    <MainComponent />
 </template>
 
 <style scoped>
---
+++ src/components/MainComponent.vue
@@ -0,0 +1,35 @@
+<script>
+import axios from 'axios';
+
+export default {
+    name: 'MainComponent',
+    data() {
+        teacherArray: []
+    },
+    computed: {
+
+    },
+    created() {
+        this.getData()
+    },
+    methods: {
+        getData() {
+            axios.get('http://localhost:8000/api/teachers')
+                .then(res => {
+                    this.teacherArray = res.data.results
+                    console.log(this.teacherArray)
+                })
+        }
+    }
+}
+</script>
+
+<template>
+    <div>
+        
+    </div>
+</template>
+
+<style scoped lang="scss">
+    
+</style>
