9fbc193bd17f3a6b4406032c5df24fb15bf623a7
marcocampisi <marco.campisi@outlook.it>
2023-10-18 12:13:37 +0200

    Add name filtering in page

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,8 @@
 <script setup>
-import HeaderComponent from './components/HeaderComponent.vue';
 import MainComponent from './components/MainComponent.vue';
 </script>
 
 <template>
-    <HeaderComponent />
     <MainComponent />
 </template>
 
--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -1,33 +1,62 @@
 <script>
 import axios from 'axios';
+import HeaderComponent from './HeaderComponent.vue';
 
 export default {
     name: 'MainComponent',
+    props: {
+        filtersTeachers: Function
+    },
     data() {
-        teacherArray: []
+        return {
+            teacherArray: [],
+            filteredTeacherArray: []
+        };
     },
-    computed: {
-
-    },
+    computed: {},
     created() {
-        this.getData()
+        this.getData();
     },
     methods: {
         getData() {
             axios.get('http://localhost:8000/api/teachers')
                 .then(res => {
-                    this.teacherArray = res.data.results
-                    console.log(this.teacherArray)
-                })
+                this.teacherArray = res.data.results;
+                console.log(this.teacherArray);
+            });
+        },
+        filterTeachers(searchQuery) {
+            this.filteredTeacherArray = this.teacherArray.filter(teacher => {
+                const fullName = teacher.first_name + ' ' + teacher.last_name;
+                return fullName.toLowerCase().includes(searchQuery.toLowerCase());
+            });
         }
-    }
+    },
+    components: { HeaderComponent }
 }
 </script>
 
 <template>
-    <div>
-        
-    </div>
+    <HeaderComponent :filterTeachers="filterTeachers" />
+    <main>
+        <div class="container">
+            <div class="row">
+                <div class="col-3 mb-3" v-for="teacher in filteredTeacherArray" :key="teacher.id">
+                    <div class="card">
+                        <div class="card-top">
+                            <img :src="teacher.full_photo_img" alt="">
+                        </div>
+                        <div class="card-body">
+                            <h2>{{ teacher.first_name }} {{ teacher.last_name }}</h2>
+                            <p>{{ teacher.bio }}</p>
+                            <p>+39 {{ teacher.phone }}</p>
+                            <h5 class="text-danger" v-if="teacher.sponsored_until">Sponsorizzato</h5>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
 </template>
 
 <style scoped lang="scss">
