6947635cd55d16596cce9ad66c0f08f86b1be538
Francescodc92 <francescodicorpo1992@hotmail.com>
2023-10-20 17:48:45 +0200

    add single page

--- src/components/HeaderSingleComponent.vue
+++ src/components/HeaderSingleComponent.vue
@@ -15,7 +15,9 @@ export default {
  
             <nav class="navbar navbar-dark navbar-expand-lg bg-transparent">
                 <div class="container-fluid">
-                    <a class="navbar-brand  text-light" href="#">BMusicTeachers</a>
+                    <a class="navbar-brand" href="#">
+                     <img class="logo" src="../assets/logo.png" alt="Musicisti in rete Logo">
+                    </a>
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
                         data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
                         aria-label="Toggle navigation">
@@ -24,7 +26,7 @@ export default {
                     <div class="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                             <li class="nav-item">
-                                <a class="nav-link active  text-light" aria-current="page" href="#">Home</a>
+                                <router-link class="nav-link  text-light" :to="{name: 'HomePage'}">Home</router-link>
                             </li>
                             <li class="nav-item">
                                 <a class="nav-link  text-light" href="#">Messaggi</a>
