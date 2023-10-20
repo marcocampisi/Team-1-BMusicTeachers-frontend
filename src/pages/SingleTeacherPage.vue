4c2e4b3e8c846078b57450c6108d02af215872e8
Francescodc92 <francescodicorpo1992@hotmail.com>
2023-10-20 10:20:16 +0200

    add link and bg

--- src/pages/SingleTeacherPage.vue
+++ src/pages/SingleTeacherPage.vue
@@ -1,16 +1,32 @@
 
 <script>
+import axios from 'axios';
 import HeaderSingleComponent from '../components/HeaderSingleComponent.vue';
 export default {
   components:{
     HeaderSingleComponent
-  }
-  
+  },
+  created() {
+    this.getSingleTeacher();
+  },
+  methods: {
+        getSingleTeacher() {
+            this.teacherArray = []
+
+            axios.get('http://127.0.0.1:8000/api/teachers/'+ this.$route.params.id)
+                .then(res => {
+                // this.teacherArray = res.data.results;
+                console.log(res.data.results);
+            });
+        },
+       
+  },
 }
 </script>
 
 <template>
   <HeaderSingleComponent/>
+  
 </template>
 
 <style lang="">
