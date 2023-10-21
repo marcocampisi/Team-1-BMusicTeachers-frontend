179cc8e6487fe92db1ff569eda1af85331616080
Francescodc92 <francescodicorpo1992@hotmail.com>
2023-10-21 23:18:23 +0200

    add single page

--- src/pages/SingleTeacherPage.vue
+++ src/pages/SingleTeacherPage.vue
@@ -3,6 +3,12 @@
 import axios from 'axios';
 import HeaderSingleComponent from '../components/HeaderSingleComponent.vue';
 export default {
+  data(){
+    return{
+      singleTeacher:null,
+      loading:false
+    }
+  },
   components:{
     HeaderSingleComponent
   },
@@ -11,12 +17,14 @@ export default {
   },
   methods: {
         getSingleTeacher() {
-            this.teacherArray = []
+            this.singleTeacher = null
+            this.loading = true
 
             axios.get('http://127.0.0.1:8000/api/teachers/'+ this.$route.params.id)
                 .then(res => {
-                // this.teacherArray = res.data.results;
-                console.log(res.data.results);
+                  this.singleTeacher = res.data.results;
+                  console.log(res.data.results);
+                  this.loading = false;
             });
         },
        
@@ -26,9 +34,18 @@ export default {
 
 <template>
   <HeaderSingleComponent/>
-  
+    <main v-if="singleTeacher && !loading" class="text-light">
+      <div class="card" style="width: 18rem;">
+        <img :src="singleTeacher.full_photo_img" class="card-img-top" :alt="singleTeacher.first_name">
+        <div class="card-body">
+          <h5 class="card-title">{{ singleTeacher.first_name }}</h5>
+          <p class="card-text">{{ singleTeacher.bio }}</p>
+          <p>+39 {{ singleTeacher.phone }}</p>
+          <h5 class="text-danger" v-if="singleTeacher.sponsorization.length > 0">Sponsorizzato</h5>
+        </div>
+      </div>
+    </main>
 </template>
 
-<style lang="">
-  
+<style scoped>
 </style>
